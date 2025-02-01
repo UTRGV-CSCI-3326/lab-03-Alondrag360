f1d3fb1cf5a4532a17c5fcf050d7669165c7ba1e
Alondrag360 <alondrag4265@gmail.com>
2025-02-01 03:10:36 +0000

    Arithmetic

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount{
+    public static void main(String[] args){
+        double x = 2175.37 - 302.50;
+        double x1 = x + 50.03;
+        double x2 = x1 / 2;
+        double x3 = x2 + 20.00;
+        double x4 = x3 - 1;
+        double x5 = x4 * 2;
+        double x6 = x5 + 1;
+        System.out.println("Bob's current balance is: " + x6);
 
+    }
+
+}
